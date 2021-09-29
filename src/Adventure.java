3907caec716a4fd6592b1d2d467af911d49d8cb6
Danish-Pirate <arja0015@stud.kea.dk>
2021-09-29 18:25:23 +0200

    Added start method

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
 
 public class Adventure {
+    int currentRoom;
 
     public static void main(String[] args) {
+        Adventure adventure = new Adventure();
         System.out.println("Welcome to Cave Game!\nType \"Start\" to begin.");
-
+        startCheck();
     }
-    public static void sayHi() {
+    public static void startCheck() {
         Scanner input = new Scanner(System.in);
-
+        boolean typedStart = false;
+        while (!typedStart) {
+            String startMessage = input.nextLine();
+            startMessage = startMessage.toLowerCase();
+            startMessage = startMessage.replaceAll(" ", "");
+            if (startMessage.equals("start")) {
+                System.out.println("Typed \"Start\"");
+                typedStart = true;
+            } else {
+                System.out.println("Didn't type \"Start\", try again.");
+            }
+        }
     }
 }
